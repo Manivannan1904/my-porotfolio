be85fd95f0e113e176b1b2058f565c6140586e77
Manivannan1904 <manivannanp0619@gmail.com>
2025-11-06 15:11:01 +0530

    Spell

--- src/components/layouts/Headers.jsx
+++ src/components/layouts/Headers.jsx
@@ -16,7 +16,7 @@ const Headers = ({
        <nav className="nav">
         <div className="nav-container">
           <div className="logo" onClick={(e) => handleNavClick(e, 'home')}>
-            Manivannan P
+            Manivannan 
           </div>
           
           <div className="nav-menu">
